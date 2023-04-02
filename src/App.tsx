727a6d82704f03266db5a4f6a9f414558e132c79
Ian <ian@hooligan.com>
2023-04-02 11:05:57 +0200

    some minor frontend changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import React, { useRef } from "react";
 import userButtons from "./user-buttons.json";
-import { Container } from "semantic-ui-react";
+import { Container, Grid, GridColumn } from "semantic-ui-react";
 import SocialButton from "./components/SocialButton";
 import Logo from "./components/Logo";
+import styled from "styled-components";
 
 const LOGO_PATH = "/TheSeaGoatLogo.png";
 
@@ -13,23 +15,86 @@ interface UserButton {
 }
 
 function App() {
+  const blobRef = useRef<HTMLDivElement>(null);
+
+  const onPointerMoveHandler = (e: any) => {
+    const { clientX, clientY } = e;
+
+    blobRef.current?.animate({
+      left: `${clientX}px`,
+      top: `${clientY}px`
+    }, { duration: 3000, fill: "forwards" });
+  }
+
+  const StyledBlob = styled.div`
+    background-color: white;
+    height: 15vmax;
+    aspect-ratio: 1;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    translate: -50% -50%;
+    border-radius: 50%;
+    background: linear-gradient(to right, rgb(33,150,243), dodgerblue);
+    animation: rotate 20s infinite;
+    opacity: 0.5;
+    z-index: -15;    
+
+    @keyframes rotate {
+      from {
+        rotate: 0deg;
+      }
+      
+      50% {
+        scale: 1 1.5;
+      }
+      
+      to {
+        rotate: 360deg;
+      }
+    }
+  `
+
+  const StyledBlur = styled.div`
+    height: 100%;
+    width: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: -2;
+    backdrop-filter: blur(8vmax);
+  `
+
   return (
-    <>
+    <div onMouseMove={onPointerMoveHandler} style={{ height: '100vh' }}>
       <Logo size="large" centered src={LOGO_PATH} fluid />
-      <Container text>
-        {userButtons.map((btn: UserButton, index) => {
-          return (
-            <SocialButton
-              key={index}
-              color={btn.color}
-              icon={btn.icon}
-              content={btn.content}
-              link={btn.link}
-            />
-          );
-        })}
-      </Container>
-    </>
+      <StyledBlob ref={blobRef} />
+      <StyledBlur />
+
+      <div style={{ zIndex: '100', marginTop: '3rem' }}>
+        <Container >
+          <Grid centered>
+            {userButtons.map((btn: UserButton, index) => {
+              return (
+                <div key={index}>
+                  <GridColumn >
+                    <a href={btn.link} target={'_blank'} rel='noreferrer'>
+                      <SocialButton
+                        key={index}
+                        color={btn.color}
+                        icon={btn.icon}
+                        content={btn.content}
+                        link={btn.link}
+                      />
+                    </a>
+                  </GridColumn>
+                </div>
+              )
+            })}
+          </Grid>
+        </Container>
+      </div>
+    </div>
   );
 }
 
